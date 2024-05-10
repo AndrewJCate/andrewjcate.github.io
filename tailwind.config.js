51d45d924b9f8db8652bd9ba0f512afe3f9f881d
OnG2FNhu7!!8g0nZ0eKFI@cX <andrewjcate@gmail.com>
2024-05-10 15:27:19 -0700

    Adds icons.

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
       colors: {
         'angular-red-light': '#dd0031',
         'angular-red-dark': '#c3002f',
+        'aws-orange': '#ff9900',
         'bootstrap-purple-light': '#8b13fc',
         'bootstrap-purple-dark': '#6b10f4',
         'css-cyan-light': '#039be5',
@@ -15,6 +16,10 @@ module.exports = {
         'docker-blue': '#1d63ed',
         'html-orange-light': '#f16529',
         'html-orange-dark': '#e44d26',
+        'intellij-pink': '#fe315d',
+        'intellij-orange': '#f97a12',
+        'intellij-purple': '#98509e',
+        'intellij-blue': '#0c7cf6',
         'invision-pink': '#dc395f',
         'java-orange': '#f5831f',
         'java-blue': '#507a98',
@@ -23,6 +28,9 @@ module.exports = {
         'mysql-blue': '#00618a',
         'mysql-orange': '#e48e00',
         'spring-green': '#6db33f',
+        'vscode-blue-light': '#39a7f1',
+        'vscode-blue-med': '#1886d0',
+        'vscode-blue-dark': '#1772b0',
       }
     },
     fontFamily: {

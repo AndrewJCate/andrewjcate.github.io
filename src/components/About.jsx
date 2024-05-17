d55123b0e2ef8371a4dce040a31ae89616272600
OnG2FNhu7!!8g0nZ0eKFI@cX <andrewjcate@gmail.com>
2024-05-17 11:02:18 -0700

    Adds About copy

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div name='about' className='w-full h-dvh bg-gradient-to-b from-gray-800 to-black text-white'>
-        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+    <div name='about' className='w-full h-full bg-gradient-to-b from-gray-800 to-black text-white'>
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center '>
 
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
@@ -13,11 +13,11 @@ const About = () => {
 
             <br></br>
 
-            <p className='text-gray-300 text-lg'>I grew up and live in the Pacific Northwest with my wife and daughter. I enjoy running, biking, reading, and PC gaming. I have a growing playing card collection. My biggest passion is learning. I spend months delving into topics that have piqued my interest.</p>
+            <p className='text-gray-300 text-lg'>I left my 8 year career in the training field to pursue my passion for programming. After several years of self teaching, I attended a university and earned my degree in Software Engineering. I created this website to showcase some of the projects I have worked on. I am excited to continue growing my skills and expanding my knowledge in this area.</p>
 
             <br></br>
 
-            <p className='text-gray-300 text-lg'>I left my 8 year career in the training field to pursue my passion for programming. After several years of self teaching, I attended a university and earned my degree in Software Engineering. I created this website to showcase some of the projects I have worked on. I am excited to continue growing my skills and expanding my knowledge in this area.</p>
+            <p className='text-gray-300 text-lg'>Here is a little personal information about me. I grew up and live in the Pacific Northwest with my wife and daughter. I enjoy running, biking, reading, and gaming. I have a growing playing card collection. My biggest passion is learning. I spend months delving into topics that have piqued my interest.</p>
 
             <br></br>
 
